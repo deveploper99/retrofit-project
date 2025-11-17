2f13d1fbacdb014464616cc62e4b6644931cb24b
deveploper99 <rtxdeveploper99gmail.com>
2025-11-16 20:21:00 -0800

    completed project retrofit

---
+++ app/src/main/java/com/example/project1/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.project1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.project1.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
